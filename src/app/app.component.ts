a895f5787103aed649df25ee6fb0f47e827ffb96
Toni Kleinfeld <54000942+gaunerleechy@users.noreply.github.com>
2025-02-12 15:02:21 +0100

    finally get rid of the arria hidden error from material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, Renderer2 } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { MatIcon } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -20,4 +20,30 @@ import { MatButtonModule } from '@angular/material/button';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent implements AfterViewInit {
+  private readonly ARIA_HIDDEN_VALUE = "false";
+
+  constructor(private renderer: Renderer2, private el: ElementRef) {}
+
+  ngAfterViewInit(): void {
+    this.setAriaHiddenLock();
+  }
+
+  private setAriaHiddenLock(): void {
+    const nativeElement = this.el.nativeElement as HTMLElement;
+    this.renderer.setAttribute(
+      nativeElement,
+      "aria-hidden",
+      this.ARIA_HIDDEN_VALUE
+    );
+
+    const originalSetAttribute = nativeElement.setAttribute.bind(nativeElement);
+
+    nativeElement.setAttribute = (attr: string, value: string) => {
+      if (attr === "aria-hidden") {
+        value = this.ARIA_HIDDEN_VALUE;
+      }
+      originalSetAttribute(attr, value);
+    };
+  }
+}
